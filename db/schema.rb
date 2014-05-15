7f65d73bc37e7c255a67de2af2f5bc6e6cf1dc5e
Amy Mok <amymok@codeforamerica.org>
2014-05-15 11:01:42 -0700

    add steps

---
+++ db/schema.rb
@@ -0,0 +1,42 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20140514182723) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "permits", force: true do |t|
+    t.string   "owner_name"
+    t.string   "owner_address"
+    t.string   "addition"
+    t.integer  "house_area"
+    t.integer  "addition_area"
+    t.string   "ac"
+    t.string   "contractor_name"
+    t.string   "contractor_id"
+    t.boolean  "escrow"
+    t.string   "license_holder"
+    t.string   "license_num"
+    t.string   "agent_name"
+    t.string   "contact_id"
+    t.string   "other_contact_id"
+    t.string   "phone"
+    t.string   "fax"
+    t.string   "email"
+    t.string   "work_summary"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
